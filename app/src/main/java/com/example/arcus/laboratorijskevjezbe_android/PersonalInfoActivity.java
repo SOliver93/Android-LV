ab4a95d456f1523083157835386474c1ccba3075
Arcus <sasa.oliver@vsmti.hr>
2018-11-15 13:09:21 +0100

    commit 2

--- app/src/main/java/com/example/arcus/laboratorijskevjezbe_android/PersonalInfoActivity.java
+++ app/src/main/java/com/example/arcus/laboratorijskevjezbe_android/PersonalInfoActivity.java
@@ -4,21 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class PersonalInfoActivity extends AppCompatActivity {
-    private EditText oImePrezime;
-    private String sImePrezime;
+    private EditText oIme;
+    public String sIme;
+    private EditText oPrezime;
+    public String sPrezime;
+    private EditText oDatum;
+    public String sDatum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
-        oImePrezime = (EditText)findViewById(R.id.etImePrezime);
+        final Button button = findViewById(R.id.btnOk);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                oIme = (EditText) findViewById(R.id.etIme);
+                oPrezime = (EditText) findViewById(R.id.etPrezime);
+                oDatum = (EditText) findViewById(R.id.etDatum);
+                sIme = oIme.getText().toString();
+                sPrezime = oPrezime.getText().toString();
+                sDatum = oDatum.getText().toString();
+                Intent Upisi = new Intent(PersonalInfoActivity.this, StudentInfoActivity.class);
+                Bundle Extras = new Bundle();
+                Extras.putString("ime", sIme);
+                Extras.putString("prezime", sPrezime);
+                Extras.putString("datum", sDatum);
+                Upisi.putExtras(Extras);
+                startActivity(Upisi);
+            }
+        });
+
     }
-    public void goToStudentInfo (View view){
-        sImePrezime = oImePrezime.getText().toString();
-        Intent Upisipredmet = new Intent (this, StudentInfoActivity.class);
-        Upisipredmet.putExtra("imePrezime", sImePrezime);
-        startActivity(Upisipredmet);
+    @Override
+    public void onBackPressed() {
+
     }
 }
--- app/src/main/java/com/example/arcus/laboratorijskevjezbe_android/StudentInfoActivity.java
+++ app/src/main/java/com/example/arcus/laboratorijskevjezbe_android/StudentInfoActivity.java
@@ -5,32 +5,65 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class StudentInfoActivity extends AppCompatActivity {
-    private EditText oUpisi;
-    private String sUpisi;
-    private String sStudent;
+    private EditText oKolegij;
+    private String sKolegij;
+    private EditText oProfesor;
+    private String sProfesor;
+    private EditText oGodina;
+    private String sGodina;
+    private EditText oPredavanja;
+    private String sPredavanja;
+    private EditText oVjezbe;
+    private String sVjezbe;
+    private TextView oIme;
+    private String sIme;
+    private TextView oPrezime;
+    private String sPrezime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
-        oUpisi = (EditText)findViewById(R.id.etKolegij);
-        final Bundle oExtras = getIntent().getExtras();
-        sStudent = oExtras.getString("imePrezime");
-    }
-
-    public void goToSummary (View view){
-        sUpisi = oUpisi.getText().toString();
-        Intent Summary = new Intent (this, SummaryActivity.class);
-        Summary.putExtra("kolegij", sUpisi);
-        Summary.putExtra("student", sStudent);
-        startActivity(Summary);
+        final Bundle PersonalInfo  = getIntent().getExtras();
+        oKolegij = (EditText)findViewById(R.id.etKolegij);
+        oProfesor = (EditText)findViewById(R.id.etProfesor);
+        oGodina = (EditText)findViewById(R.id.etGodina);
+        oPredavanja = (EditText)findViewById(R.id.etPredavanja);
+        oVjezbe = (EditText)findViewById(R.id.etVjezbe);
+        sIme = PersonalInfo.getString("ime");
+        sPrezime = PersonalInfo.getString("prezime");
+        oIme = (TextView)findViewById(R.id.tvIme);
+        oPrezime = (TextView)findViewById(R.id.tvPrezime);
+        oIme.setText(sIme);
+        oPrezime.setText(sPrezime);
+        final Button button = findViewById(R.id.btnDalje);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sKolegij = oKolegij.getText().toString();
+                sProfesor = oProfesor.getText().toString();
+                sGodina = oGodina.getText().toString();
+                sPredavanja = oPredavanja.getText().toString();
+                sVjezbe = oVjezbe.getText().toString();
+                Bundle Extras = new Bundle();
+                Extras.putString("kolegij", sKolegij);
+                Extras.putString("profesor", sProfesor);
+                Extras.putString("godina", sGodina);
+                Extras.putString("predavanja", sPredavanja);
+                Extras.putString("vjezbe", sVjezbe);
+                Intent Summary = new Intent(StudentInfoActivity.this, SummaryActivity.class);
+                Summary.putExtras(Extras);
+                Summary.putExtras(PersonalInfo);
+                startActivity(Summary);
+            }
+        });
     }
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-        return (keyCode == KeyEvent.KEYCODE_BACK ? true : super.onKeyDown(keyCode, event));
+    public void onBackPressed() {
+
     }
 }
