9b5260132cd4201004bbfadabe39baf995ab00be
SOliver93 <sasa.oliver@vsmti.hr>
2018-11-22 18:57:36 +0100

    commit 5

---
+++ app/src/main/java/com/example/arcus/laboratorijskevjezbe_android/FragmentViewPagerAdapter.java
@@ -0,0 +1,13 @@
+package com.example.arcus.laboratorijskevjezbe_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentViewPagerAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_view_pager_adapter);
+    }
+}
